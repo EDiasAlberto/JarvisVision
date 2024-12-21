3cb7fc1a563e25b590c7c2a750e6069a34f9224f
EDiasAlberto <proxamon1@gmail.com>
2024-12-21 21:53:53 +0000

    Implement computer vision testing functions and sending images to chatGPT

--- computer.py
+++ computer.py
@@ -1,4 +1,6 @@
-from cv2 import *
+from cv2 import VideoCapture, imshow, imwrite, namedWindow, waitKey, imencode
+from base64 import b64encode
+from time import sleep
 import xerox
 
 #handles any interactions with the computer
@@ -11,10 +13,19 @@ class Computer:
 
     def __init__(self):
         self.cam = VideoCapture(0)
+        namedWindow("outputFrame")
+
+    def testCamera(self):
+        s, img = self.cam.read()
+        imshow("outputFrame", img)
+        imwrite("output.jpg", img)
+        sleep(10)
 
     def takePhoto(self):
         s, img = self.cam.read()
         if s:
+            s, buffer = imencode(".jpg", img)
+            b64 = b64encode(buffer)
             return img
 
     def saveTextFile(self, contents, path):
@@ -28,3 +39,8 @@ class Computer:
             return
         return contents
 
+
+if __name__=="__main__":
+    client = Computer()
+
+    client.testCamera()
--- gpt.py
+++ gpt.py
@@ -2,6 +2,7 @@ from openai import OpenAI
 
 from dotenv import load_dotenv
 from os import getenv
+from base64 import b64encode
 
 setupContext = [
     "You are a helpful AI assistant. Refer to the user as Sir. Your purpose is to aid in project guidance or programming help.",
@@ -37,14 +38,32 @@ class GPT:
         response = self.sendMessageHistory()
         return response
 
+    def sendImg(self, imgData):
+        imgMsg = {"role": "user", "content": [
+            {
+                "type": "text",
+                "text": "Here is the associated image for the previous request",
+            },
+            {
+                "type": "image_url",
+                "image_url": {"url": f"data:image/jpeg;base64,{imgData}"}
+            }
+        ]}
+        self.messages.append(imgMsg)
+        response = self.sendMessageHistory()
+        return response
+
 
 if __name__=="__main__":
     load_dotenv()
     key = getenv("OPENAI_API_KEY")
     client = GPT(key)
 
-    client.askGPT("Hey, what is your purpose?")
-    client.askGPT("Cool, how would you write a HTTP request in Python?")
-    client.askGPT("What was my first message to you?")
-    client.askGPT("Please make a list of all the items on my desk")
-
+    #client.askGPT("Hey, what is your purpose?")
+    #client.askGPT("Cool, how would you write a HTTP request in Python?")
+    #client.askGPT("What was my first message to you?")
+    client.askGPT("Please describe the image im sending to you")
+    b64Img = 0
+    with open("forest.jpg", "rb") as image:
+        b64Img = b64encode(image.read()).decode("utf-8")
+    client.sendImg(b64Img)
